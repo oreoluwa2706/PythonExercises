74c780fe1f9eb758447f437a8bc4a0879a43d836
oreoluwa2706 <113597621+oreoluwa2706@users.noreply.github.com>
2022-10-06 16:16:41 +0100

    Add files via upload

---
+++ equilateral_traingle.py
@@ -0,0 +1,7 @@
+side_1 = int(input('side 1 is: '))
+side_2 = int(input('side 2 is: '))
+side_3 = int(input('side 3 is: '))
+if side_1 == side_2 and side_2 == side_3:
+    print('This is an equilateral triangle')
+else:
+    print('This is not an equilateral triangle')
